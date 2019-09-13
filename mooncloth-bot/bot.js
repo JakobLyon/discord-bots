49babaced7ed1d0e4224f74ccf9ec479e78dc050
JakobLyon <jlyon32893@gmail.com>
2019-09-13 16:26:28 -0400

    implemented user ability to cancel reminders, cleaned up time formating for user

--- mooncloth-bot/bot.js
+++ mooncloth-bot/bot.js
@@ -151,16 +151,16 @@ bot.on("message", function(user, userID, channelID, message, evt) {
           });
         }).then(() => {
           const moonclothTimer = moment().add(MOONCLOTH_CD_DAYS, "days");
-          const earlyReminder = moonclothTimer.subtract(
+          const earlyReminder = moment(moonclothTimer).subtract(
             EARLY_CD_REMINDER_DAYS,
             "days"
           );
-          const recurringReminder = moonclothTimer.add(
+          const recurringReminder = moment(moonclothTimer).add(
             POST_CD_REMINDER_HOURS,
             "hours"
           );
 
-          const moonclothMessage = `Cooldown available on ${moonclothTimer.format()}`;
+          const moonclothMessage = `Cooldown available on ${moonclothTimer.format("LL LT")}`;
           moonclothLogger.info(moonclothMessage, {
             user,
             cooldownAvailable: moonclothTimer
@@ -168,7 +168,9 @@ bot.on("message", function(user, userID, channelID, message, evt) {
           // Early reminder
           eventsLogger.info({
             userID,
-            message: `Your mooncloth will be ready to craft on ${earlyReminder.format("LL")} ${earlyReminder.format("LT")}`,
+            message: `Your mooncloth will be ready to craft on ${earlyReminder.format(
+              "LL"
+            )} ${earlyReminder.format("LT")}`,
             reminderTime: earlyReminder.format(),
             id: createGUID(),
             channelID
@@ -240,7 +242,53 @@ bot.on("message", function(user, userID, channelID, message, evt) {
         });
         break;
       case "mooncloth cancel":
+        const eventSearchOptions = {
+          fields: [
+            "userID",
+            "id",
+            "reminderTime",
+            "channelID",
+            "message",
+            "reminderInterval",
+            "type"
+          ],
+          from: moment().subtract(10, "days")
+        };
+        eventsLogger.query(eventSearchOptions, (err, results) => {
+          const toDoEvents = results.file.filter(result => {
+            return (
+              moment(result.reminderTime).format("YYYYMMDDHHmmss") >
+                moment().format("YYYYMMDDHHmmss") && result.userID === userID
+            );
+          });
 
+          const eventCancelsOptions = {
+            fields: ["id"],
+            from: moment().subtract(10, "days")
+          };
+
+          eventCancelsLogger.query(eventCancelsOptions, (err, results) => {
+            const canceledEvents = results.file.map(result => result.id);
+
+            toDoEvents.forEach(event => {
+              if (!canceledEvents.includes(event.id)) {
+                // do event
+                bot.sendMessage({
+                  to: event.channelID,
+                  message: `<@${event.userID}>, reminder for ${moment(
+                    event.reminderTime
+                  ).format("LL LT")} has been canceled`
+                });
+
+                // cancel event
+                eventCancelsLogger.info("", {
+                  id: event.id,
+                  userID: event.userID
+                });
+              }
+            });
+          });
+        });
         break;
       case "wow spreadsheet":
         bot.sendMessage({
